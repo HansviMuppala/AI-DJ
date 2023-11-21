fa376cdda1f91ab1d219cdaaee128b5fbbcaccde
Hansvi <123161387+HansviMuppala@users.noreply.github.com>
2023-11-21 19:24:46 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 song_one = "";
 song_two = "";
+song_status = "";
 
 leftwristX = 0;
 leftwristY = 0;
+
 rightwristX = 0;
 rightwristY = 0;
+
+scoreLeftWrist = 0;
 
 function preload(){
     song_one = loadSound("music.mp3");
@@ -24,6 +28,21 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    
+    fill("FF0000");
+    stroke("FF0000");
+    
+    song_status = song_one.isPlaying();
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftwristX, leftwristY, 30);
+        song_two.stop()
+    }
+
+    if(song_status = false){
+        song_one.play();
+        document.getElementById("song").innerHTML = "Song Title = " + song_one;
+    }
 }
 
 function modelLoaded(){
@@ -33,8 +52,12 @@ function modelLoaded(){
 function gotPoses(){
     if(results.length > 0){
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+
         leftwristX = results[0].pose.leftwrist.x;
         leftwristY = results[0].pose.leftwrist.y;
+        
         rightwristX = results[0].pose.rightwrist.x;
         rightwristY = results[0].pose.rightwrist.y;
     }
